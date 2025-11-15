9e2519223582c02d51bb4bc84b4dbc801c74812e
Tsoeu14 <htsoeu@byupatthway.edu>
2025-11-15 23:54:22 +0200

    tough times

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,13 @@
-const button = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
+function handleMenuDisplay(){
+    const menu = document.getElementById('menu');
+    const screenWidth = window.innerWidth;
 
-button.addEventListener('click', () => {
-    menu.classList.toggle(open);
-    button.textContent = menu.classList.contains('open') ? 'x' : '&#9776';
-} )
+    if (screenWidth >= 1024) {
+        menu.style.display = 'block';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+window.addEventListener('load', handleMenuDisplay);
+window.addEventListener('resize', handleMenuDisplay);
