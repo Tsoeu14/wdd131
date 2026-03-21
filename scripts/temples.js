87659242b707f5ab2f7e3b91bb663140671dccca
tsoeu14 <htsoeu@byupathway.edu>
2026-03-21 17:25:09 +0200

    temple updte

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,8 @@
 document.getElementById("currentyear").textContent = new Date().getUTCFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
-function handleMenuDisplay() {
-    const menu = document.getElementById('menu');
-    const screenWidth = window.innerWidth;
 
-    if (screenWidth >= 1024) {
-        menu.style.display = 'block';
-    } else {
-        menu.style.display = 'none';
-    }
-}
-
-window.addEventListener('load', handleMenuDisplay);
-window.addEventListener('resize', handleMenuDisplay);
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("open");
+}); 
